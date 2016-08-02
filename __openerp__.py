b6a20b875133456ae22f3d207589caba50b30df4
Yannick Buron <yannick.buron@gmail.com>
2016-08-02 22:44:25 +0200

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Theme Unify',
+    'category': 'Website',
+    'version': '1.0',
+    'description': """
+        """,
+    'author': 'Yannick Buron (Clouder)',
+    'depends': ['website'],
+    'data': [
+        'views/assets.xml',
+        'views/headers.xml',
+        'views/themes.xml',
+    ],
+    # 'qweb': ['static/src/xml/header.xml'],
+    'installable': True,
+    'application': False,
+}
